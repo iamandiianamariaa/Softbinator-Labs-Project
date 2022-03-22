22e34b638c9c17103f816499c9c7ff656babafca
iamandiianamariaa <iamandiianamariaa@gmail.com>
2022-03-22 16:17:43 +0200

    First commit

---
+++ src/test/java/com/example/charity_app/CharityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.charity_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
